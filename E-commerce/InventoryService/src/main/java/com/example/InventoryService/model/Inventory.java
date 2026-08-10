03e048082201b834020d9f425ca0f3d7cb0f398e
vaibhav nimbalkar <nimbalkarvaibhav358@gmail.com>
2026-08-10 17:20:14 +0530

    My first commit

---
+++ E-commerce/InventoryService/src/main/java/com/example/InventoryService/model/Inventory.java
@@ -0,0 +1,30 @@
+package com.example.InventoryService.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer availableQuantity;
+
+    private Integer reservedQuantity;
+
+    private Integer reorderLevel;
+
+    private Boolean inStock;
+}
